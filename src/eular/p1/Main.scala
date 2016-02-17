84bdef766cc5b9e3e1eef58736e743210ff0f485
shunsuke.tada <shunsuke.tada.fe@ps.hitachi-solutions.com>
2016-02-17 11:11:56 +0900

    problem 1

---
+++ src/eular/p1/Main.scala
@@ -0,0 +1,31 @@
+package eular.p1
+
+import util.Measure
+
+object Main {
+
+  def main(args: Array[String]) : Unit = {
+    println("answer1")
+    Measure.ms(answer1)
+
+    println("-------------------")
+    println("answer2")
+    Measure.ms(answer2)
+  }
+
+  def answer1() = {
+    val answer = List.range(1, 1000)
+      .filter(n => (n%3 == 0) || (n%5 == 0))
+      .reduce((total, n) => total + n)
+
+    println(s"""answer:${ answer }""")
+  }
+
+  def answer2() = {
+    val answer = List.range(1, 1000)
+      .filter(n => (n%3 == 0) || (n%5 == 0))
+      .sum
+
+    println(s"""answer:${ answer }""")
+  }
+}
---
+++ src/util/Measure.scala
@@ -0,0 +1,19 @@
+package util
+
+import scala.concurrent._
+import scala.concurrent.duration.Duration
+import ExecutionContext.Implicits.global
+
+object Measure {
+  def ms[A](f : => A) {
+    val start = System.currentTimeMillis()
+    val task: Future[Unit] = Future {
+      f
+
+      val end = System.currentTimeMillis()
+      println(s"""${ end - start }ms""")
+    }
+
+    Await.ready(task, Duration.Inf)
+  }
+}
