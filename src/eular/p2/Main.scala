93ee40ee195cf2e3ff98ca6740f326c2eac4fd21
ppa <dummy@mail.com>
2016-02-17 15:51:23 +0900

    problem 2

---
+++ src/eular/p2/Main.scala
@@ -0,0 +1,21 @@
+package eular.p2
+
+import util.Measure
+
+object Main {
+  def main(args : Array[String]) = {
+    Measure.ms(answer1)
+  }
+
+  def answer1() = {
+    val fs: Stream[Int] = fibFrom(1, 1)
+
+    val answer = fs.takeWhile( x => x < 4000000 )
+      .filter(_ % 2 == 0)
+      .sum
+
+    println(answer)
+  }
+
+  def fibFrom(a: Int, b:Int): Stream[Int] = a #:: fibFrom(b, a + b)
+}
