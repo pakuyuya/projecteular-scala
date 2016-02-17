3985a09d3f029b20eff9bf8bf28bbac976f498fa
ppa <dummy@mail.com>
2016-02-17 15:51:49 +0900

    problem 4

---
+++ src/eular/p4/Main.scala
@@ -0,0 +1,48 @@
+package eular.p4
+
+import util.Measure
+
+object Main {
+  def main(args: Array[String]) = {
+    Measure.ms(() => println("problem 4"))
+
+    Measure.ms(answer1)
+
+    Measure.ms(godSaid)
+  }
+
+  def answer1() = {
+
+    def isPalindromic(n:Int):Boolean = {
+      val str = n.toString()
+
+      val invalid_char = str.zipWithIndex.find {
+        case(c:Char, i:Int) =>
+          c != str.charAt(str.length()-1-i)
+      }
+
+      invalid_char match {
+        case Some(_) => false
+        case None    => true
+      }
+    }
+
+    // ｶﾐｻﾏに教えてもらいました
+    val answer = (100 to 999)
+      .flatMap(i => (i to 999).map(_ * i))
+      .filter(isPalindromic(_))
+      .max
+
+    println(s"""answer: ${ answer }""");
+  }
+
+  def godSaid() = {
+    // ﾎｹﾞｪ
+    val answer = (100 to 999)
+      .flatMap(i => (i to 999).map(_ * i))
+      .filter(i => i.toString() == i.toString().reverse)
+      .max
+
+      println(s"""answer: ${ answer }""");
+}
+}
