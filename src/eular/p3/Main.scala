372938a5ec3d86ac26e8b676d8ca58df4c542c7a
ppa <dummy@mail.com>
2016-02-17 15:51:33 +0900

    problem3

---
+++ src/eular/p3/Main.scala
@@ -0,0 +1,44 @@
+package eular.p3
+
+import util.Measure
+import Math.sqrt
+
+object Main {
+  def main(args : Array[String]) = {
+
+    Measure.ms(()=> println("init"))
+
+    println("answer1")
+    Measure.ms(answer1)
+
+    println("-------------------")
+    println("god said")
+    Measure.ms(godSaid)
+  }
+
+
+  def godSaid() = {
+    def factors(n: Long): List[Long] =
+      (2 to Math.sqrt(n).toInt)
+        .find(n % _ == 0)
+        .fold(List(n))(i => i.toLong:: factors(n / i))
+
+
+    val answer = factors(600851475143L).last
+
+    println(s"""answer: ${ answer }""")
+  }
+  def answer1() = {
+
+    var base:Long = 600851475143L
+
+    def nextFactors(i:Int, n: Long): List[Long] =
+      (i to Math.sqrt(n).toInt)
+        .find(n % _ == 0)
+        .fold(List(n))(i => i.toLong:: nextFactors(i, n / i))
+
+    var answer = nextFactors(2, base).last;
+
+    println(s"""answer: ${ answer }""")
+  }
+}
