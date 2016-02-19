4d5aee796c874cc4cb8b96c33755715428064e1c
ppa <dummy@mail.com>
2016-02-19 11:20:10 +0900

    problem 6

---
+++ src/eular/p6/Main.scala
@@ -0,0 +1,37 @@
+package eular.p6
+
+import util.Measure
+import Math.pow
+import Math.abs
+
+object Main {
+
+  def main(args: Array[String]) = {
+    Measure.ms(() => println("problem 6"))
+
+    println("answer1");
+    Measure.ms(answer1)
+  }
+
+  /**
+   * シンプルに
+   */
+  def answer1() = {
+
+    val squaresum = (1 to 100).map(i => i * i).sum
+    val sum = (1 to 100).sum
+
+    val answer = abs(squaresum - sum * sum)
+
+    println(s"answer: ${ answer }")
+  }
+
+  def godsaid() = {
+    // なるほどもなてぃっく。
+    def square = (n: Int) => n * n
+    val answer = square((1 to 100).sum) - (1 to 100).map(square).sum
+
+    println(s"answer: ${ answer }")
+  }
+
+}
